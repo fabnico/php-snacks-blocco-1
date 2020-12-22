78ab9e9c5a89f121deda9ca301039d69544c82da
Fabrizio <whoet.wo@gmail.com>
2020-12-22 18:34:24 +0100

    Snack 2 completato

---
+++ snack-2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+   $name = $_GET["name"];
+   $mail = $_GET["mail"];
+   $age = $_GET["age"];
+
+   if (strlen($name) > 3 && strstr($mail, '@') && strstr($mail, '.') && is_numeric($age)){
+      echo "Accesso riuscito";
+   } else {
+      echo "Accesso negato";
+   }
+   
+?>
